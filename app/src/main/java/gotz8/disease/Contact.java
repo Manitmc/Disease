4fb348acea1388e0bea4aff3006a93521f1d5f41
Manitmc <manit11799@gmail.com>
2018-03-27 23:51:17 +0700

    initial commit

--- app/src/main/java/gotz8/disease/Contact.java
+++ app/src/main/java/gotz8/disease/Contact.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,20 +27,58 @@ public class Contact extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact1);
-        final TextView cdc =  findViewById(R.id.textView41);
+
+        final ImageView p2 =  findViewById(R.id.p2);
+        final ImageView p3 =  findViewById(R.id.p3);
+        final ImageView p4 =  findViewById(R.id.p4);
+        final ImageView p5 =  findViewById(R.id.p5);
 
 
-        cdc.setOnClickListener(new View.OnClickListener() {
+        p4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String url1 = "https://www.google.co.th/maps/search/center+for+disease+control/@33.7993178,-84.3301967,17z/data=!3m1!4b1?dcr=0";
+                String url2 = "https://www.facebook.com/CDC";
 
                 CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
                 builder.setToolbarColor(ContextCompat.getColor(Contact.this, R.color.colorPrimary));
                 builder.setShowTitle(true);
-                builder.setActionButton(BitmapFactory.decodeResource(getResources(), R.drawable.arrow), "Share", Extension.createPendingShareIntent(Contact.this));
+                builder.setActionButton(BitmapFactory.decodeResource(getResources(), R.drawable.arrow_s), "Share", Extension.createPendingShareIntent(Contact.this));
+                builder.build().launchUrl(Contact.this, Uri.parse(url2));
+            }
+        });
+        p3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String url1 = "https://www.instagram.com/CDCgov/";
+
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.setToolbarColor(ContextCompat.getColor(Contact.this, R.color.colorPrimary));
+                builder.setShowTitle(true);
+                builder.setActionButton(BitmapFactory.decodeResource(getResources(), R.drawable.arrow_s), "Share", Extension.createPendingShareIntent(Contact.this));
                 builder.build().launchUrl(Contact.this, Uri.parse(url1));
             }
         });
+        p2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String url3 = "https://www.youtube.com/user/CDCstreamingHealth";
+
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.setToolbarColor(ContextCompat.getColor(Contact.this, R.color.colorPrimary));
+                builder.setShowTitle(true);
+                builder.setActionButton(BitmapFactory.decodeResource(getResources(), R.drawable.arrow_s), "Share", Extension.createPendingShareIntent(Contact.this));
+                builder.build().launchUrl(Contact.this, Uri.parse(url3));
+            }
+        });
+        p5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String url4 = "https://twitter.com/CDCgov";
+
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                builder.setToolbarColor(ContextCompat.getColor(Contact.this, R.color.colorPrimary));
+                builder.setShowTitle(true);
+                builder.setActionButton(BitmapFactory.decodeResource(getResources(), R.drawable.arrow_s), "Share", Extension.createPendingShareIntent(Contact.this));
+                builder.build().launchUrl(Contact.this, Uri.parse(url4));
+            }
+        });
+
 
     }
     public void ClickContact (View view) {
